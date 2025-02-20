85d7f43d952243fc51689dbd5f9d97d0b01dbbe8
ALI MAHDI <126531731+Alilo2005@users.noreply.github.com>
2025-02-20 00:18:35 +0000

    Refactor bot initialization by moving index.js to the root directory

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { token } from './config.json';
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
